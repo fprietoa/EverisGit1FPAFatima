bfa64af9d09a191ec1d5d7e8efdeb8ecffe5a8a8
fprietoa <fprietoa@everis.com>
2021-03-25 10:35:34 +0100

    Version 1 - GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,24 @@
+package com.everis.git;
+
+/**
+ * Formacion Dual - Git
+ * 
+ * @author fprietoa
+ *
+ */
+
+public class AppMain {
+	
+	/**
+	 * Metodo prinicpal de la clase
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+
+	}
+
+}
